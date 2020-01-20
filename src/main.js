1bfaf7cf42e35651a85ef75803b1aa6061474862
kniedzie <kniedzie@d17-l427-pc-10.ipa.ki.agh.edu.pl>
2020-01-20 09:34:03 +0100

    cos nie dziala

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import Meetings from './Meetings.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import VueResource from "vue-resource";
 
+Vue.use(VueResource);
 new Vue({
   render: h => h(Meetings),
 }).$mount('#app');
+Vue.http.options.root = '/api';
